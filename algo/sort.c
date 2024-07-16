750de7a9672833ea1b1ee83ce464b728fbbf1a21
Hossamfazaz0 <hossamfazaz6@gmail.com>
2024-07-16 18:31:41 +0100

    trying to fix norminette

--- algo/sort.c
+++ algo/sort.c
@@ -11,87 +11,100 @@
 /* ************************************************************************** */
 
 #include "../push_swap.h"
-#include <limits.h>
-#include <stdio.h>
 
-int list_length(t_stack *stack) {
-    int length = 0;
-    t_stack *current = stack;
-    while (current) {
-        length++;
-        current = current->next;
-    }
-    return length;
+int	list_length(t_stack *stack)
+{
+	int		length;
+	t_stack	*current;
+
+	length = 0;
+	current = stack;
+	while (current)
+	{
+		length++;
+		current = current->next;
+	}
+	return (length);
 }
 
-int get_max(t_stack *stack) {
-    int max = INT_MIN;
-    t_stack *current = stack;
-    while (current) {
-        if (current->data > max)
-            max = current->data;
-        current = current->next;
-    }
-    return max;
+int	get_max(t_stack *stack)
+{
+	int		max;
+	t_stack	*current;
+
+	max = INT_MIN;
+	current = stack;
+	while (current)
+	{
+		if (current->data > max)
+			max = current->data;
+		current = current->next;
+	}
+	return (max);
 }
 
-void sort(t_stack **sa, t_stack **sb, int *tab, int tab_size,int max) {
-    int start = 0;
-    int end = max;
+void	sort(t_stack **sa, t_stack **sb, int *tab, int tab_size, int max)
+{
+	int		start;
+	int		end;
+	int		max_val;
+	int		middle;
+	int		max_index;
+	t_stack	*current;
 
-    while (*sa != NULL) {
-        if ((*sa)->data >= tab[start] && (*sa)->data <= tab[end]) {
-            push_b(sa, sb);
-            start++;
-            end++;
-            if (end >= tab_size) {
-                end = tab_size - 1;
-            }
-        } else if ((*sa)->data < tab[start]) {
-            push_b(sa, sb);
-            rotate_b(sb);
-            start++;
-            end++;
-            if (end >= tab_size) {
-                end = tab_size - 1;
-            }
-        } else if ((*sa)->data > tab[end]) {
-            rotate_a(sa);
-        }
-    }
-
-    while (*sb != NULL) {
-        int max_val = get_max(*sb);
-        int middle = list_length(*sb) / 2;
-        int max_index = 0;
-        t_stack *current = *sb;
-
-        while (current) {
-            if (current->data == max_val)
-                break;
-            max_index++;
-            current = current->next;
-        }
-
-        if (max_index <= middle) {
-            while ((*sb)->data != max_val)
-                rotate_b(sb);
-        } else {
-            while ((*sb)->data != max_val)
-                rotate_reverse_b(sb);
-        }
-        push_a(sa, sb);
-    }
+	start = 0;
+	end = max;
+	while (*sa != NULL)
+	{
+		if ((*sa)->data >= tab[start] && (*sa)->data <= tab[end])
+		{
+			push_b(sa, sb);
+			start++;
+			end++;
+			if (end >= tab_size)
+			{
+				end = tab_size - 1;
+			}
+		}
+		else if ((*sa)->data < tab[start])
+		{
+			push_b(sa, sb);
+			rotate_b(sb);
+			start++;
+			end++;
+			if (end >= tab_size)
+			{
+				end = tab_size - 1;
+			}
+		}
+		else if ((*sa)->data > tab[end])
+		{
+			rotate_a(sa);
+		}
+	}
+	while (*sb != NULL)
+	{
+		max_val = get_max(*sb);
+		middle = list_length(*sb) / 2;
+		max_index = 0;
+		current = *sb;
+		while (current)
+		{
+			if (current->data == max_val)
+				break ;
+			max_index++;
+			current = current->next;
+		}
+		if (max_index <= middle)
+		{
+			while ((*sb)->data != max_val)
+				rotate_b(sb);
+		}
+		else
+		{
+			while ((*sb)->data != max_val)
+				rotate_reverse_b(sb);
+		}
+		push_a(sa, sb);
+	}
 }
-    // t_stack *current = *sa;
-//     while (current) {
-//         printf("%d\n", current->data);
-//         current = current->next;
-//     }
-
-//     current = *sb;
-//     while (current) {
-//         printf(" %d\n", current->data);
-//         current = current->next;
-//     } 
-
--- algo/sort_three.c
+++ algo/sort_three.c
@@ -1,27 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sort_three.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hfazaz <hfazaz@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/16 18:23:42 by hfazaz            #+#    #+#             */
+/*   Updated: 2024/07/16 18:27:21 by hfazaz           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../push_swap.h"
-void sort_three(t_stack **sa)
+
+void	sort_three(t_stack **sa)
 {
-    if (!*sa || !(*sa)->next || !(*sa)->next->next)
-        return;
+	int	a;
+	int	b;
+	int	c;
 
-    t_stack *first = *sa;
-    t_stack *second = (*sa)->next;
-    t_stack *third = (*sa)->next->next;
-
-    if (first->data > second->data && second->data < third->data && first->data < third->data)
-        swap_a(sa);
-    else if (first->data > second->data && second->data > third->data)
-    {
-        swap_a(sa);
-        rotate_reverse_a(sa);
-    }
-    else if (first->data > second->data && second->data < third->data && first->data > third->data)
-        rotate_a(sa);
-    else if (first->data < second->data && second->data > third->data && first->data < third->data)
-    {
-        swap_a(sa);
-        rotate_a(sa);
-    }
-    else if (first->data < second->data && second->data > third->data && first->data > third->data)
-        rotate_reverse_a(sa);
+	if (!*sa || !(*sa)->next || !(*sa)->next->next)
+		return ;
+	a = (*sa)->data;
+	b = (*sa)->next->data;
+	c = (*sa)->next->next->data;
+	if (a > b && b < c && a < c)
+		swap_a(sa);
+	else if (a > b && b > c)
+	{
+		swap_a(sa);
+		rotate_reverse_a(sa);
+	}
+	else if (a > b && b < c && a > c)
+		rotate_a(sa);
+	else if (a < b && b > c && a < c)
+	{
+		swap_a(sa);
+		rotate_a(sa);
+	}
+	else if (a < b && b > c && a > c)
+		rotate_reverse_a(sa);
 }
